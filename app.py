6771b47629130ffbcdac185ead4bcce7e10e4b54
WahabAsifMemon <wahabasifdeveloper@gmail.com>
2024-07-11 18:10:07 +0500

    pdf to power point

--- app.py
+++ app.py
@@ -286,43 +286,7 @@ if 'DYNO' in os.environ:  # Running on Heroku
 else:
     config = pdfkit.configuration()
 
-@app.route('/upload-html-to-pdf', methods=['POST'])
-def upload_html_to_pdf():
-    try:
-        if 'file' not in request.files:
-            logging.error('No file part in the request')
-            return jsonify({'error': 'No file part'}), 400
 
-        file = request.files['file']
-
-        if file.filename == '':
-            logging.error('No selected file')
-            return jsonify({'error': 'No selected file'}), 400
-
-        if file and allowed_file(file.filename, ALLOWED_EXTENSIONS):
-            clear_folder(UPLOAD_FOLDER)
-            clear_folder(OUTPUT_FOLDER)
-
-            file_path = os.path.join(UPLOAD_FOLDER, secure_filename(file.filename))
-            file.save(file_path)
-
-            if file_path.endswith('.html'):
-                pdf_file_path = os.path.join(OUTPUT_FOLDER, f"{os.path.splitext(file.filename)[0]}.pdf")
-
-                # Convert HTML to PDF using pdfkit
-                pdfkit.from_file(file_path, pdf_file_path, configuration=config)
-
-                return jsonify({'filename': f"{os.path.splitext(file.filename)[0]}.pdf"}), 200
-            elif file_path.endswith('.pdf'):
-                return jsonify({'filename': f"{os.path.splitext(file.filename)[0]}.pdf"}), 200
-
-        else:
-            logging.error('Invalid file type, only HTML or PDF files are allowed')
-            return jsonify({'error': 'Invalid file type, only HTML or PDF files are allowed'}), 400
-
-    except Exception as e:
-        logging.error(f'Error during file upload: {e}')
-        return jsonify({'error': f'File upload failed: {str(e)}'}), 500
 
 
 # CONVERTING PDF TO PDF/A
